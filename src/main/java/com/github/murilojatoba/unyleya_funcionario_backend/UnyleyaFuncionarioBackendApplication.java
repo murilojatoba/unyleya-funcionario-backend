6262fd95ab6bceeb885e0956d49cd7ada8cb9a76
Murilo Jatobá <murilo_75517@trt5.jus.br>
2025-01-28 17:13:35 -0300

    Inclusão do projeto importado do Spring Initializr.

---
+++ src/main/java/com/github/murilojatoba/unyleya_funcionario_backend/UnyleyaFuncionarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.murilojatoba.unyleya_funcionario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnyleyaFuncionarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnyleyaFuncionarioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/murilojatoba/unyleya_funcionario_backend/UnyleyaFuncionarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.murilojatoba.unyleya_funcionario_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnyleyaFuncionarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
